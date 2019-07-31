6f406a126626dbe8f40f72d0112a4140f0cfb993
nicolasmarcos <nicolasmarcos.ti@gmail.com>
2019-07-31 11:03:51 -0300

    Persistência automática em H2 de categorias para desenvolvimento e testes

--- src/main/java/com/nicolasmarcosti/cursomc/CursomcApplication.java
+++ src/main/java/com/nicolasmarcosti/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.nicolasmarcosti.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.nicolasmarcosti.cursomc.domain.Categoria;
+import com.nicolasmarcosti.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
--- src/main/java/com/nicolasmarcosti/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nicolasmarcosti/cursomc/resources/CategoriaResource.java
@@ -32,8 +32,7 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 		
 		/*
-		Categoria cat1 = new Categoria(1,"Informática");
-		Categoria cat2 = new Categoria(2,"Escritório");
+		
 		
 		List<Categoria> lista = new ArrayList<>();
 		lista.add(cat1);
